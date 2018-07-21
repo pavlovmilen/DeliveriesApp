57cfe4349d04a0888cd96f4e5aed19b1f4a01775
Milen Pavlov <milenppavlov@gmail.com>
2018-07-21 18:11:23 +0100

    18-5

---
+++ DeliveriesApp/DeliveriesApp/Models/Delivery.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+
+namespace DeliveriesApp.Models
+{
+    public class Delivery
+    {
+        public string Id { get; set; }
+
+        public static async Task<List<Delivery>> GetDeliveries()
+        {
+            var deliveries = await AzureHelper.MobileService.GetTable<Delivery>().ToListAsync();
+
+            return deliveries;
+        }
+    }
+}
---
+++ DeliveriesApp/DeliveriesApp/Models/DeliveryPerson.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveriesApp.Models
+{
+    public class DeliveryPerson
+    {
+        public string Id { get; set; }
+
+        public static async Task<DeliveryPerson> GetDeliveryPerson(string id)
+        {
+            var deliveryPerson = (await AzureHelper.MobileService.GetTable<DeliveryPerson>().Where(d => d.Id == id).ToListAsync())
+                .FirstOrDefault();
+
+            return deliveryPerson;
+        }
+    }
+}
