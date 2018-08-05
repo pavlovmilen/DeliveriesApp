7afe46ad2649e3c0a69ba00b59d6c17b346b98e5
Milen Pavlov <milenppavlov@gmail.com>
2018-08-05 15:18:59 +0100

    25-1-1

---
+++ DeliveryPersonApp.Android/PickUpActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DeliveryPersonApp.Android
+{
+    [Activity(Label = "PickUpActivity")]
+    public class PickUpActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.PickUp);
+        }
+    }
+}
