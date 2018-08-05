22770e7fbe19051ac9aa21f899caad07f8ea6215
Milen Pavlov <milenppavlov@gmail.com>
2018-08-05 16:37:59 +0100

    25-1-3

--- DeliveryPersonApp.Android/TabsActivity.cs
+++ DeliveryPersonApp.Android/TabsActivity.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
@@ -12,15 +7,14 @@ using Android.Widget;
 
 namespace DeliveryPersonApp.Android
 {
-    [Activity(Label = "DeliverActivity")]
-    public class DeliverActivity : Activity
+    [Activity(Label = "TabsActivity")]
+    public class TabsActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
-            SetContentView(Resource.Layout.Deliver);
         }
     }
 }
